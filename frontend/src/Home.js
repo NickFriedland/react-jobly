74b4d6b05f60fa36ffa57c6d012b2108d80d5f17
Nick Friedland <nick.friedland@gmail.com>
2019-01-16 17:00:11 -0800

    front end create companies, jobs, some styling

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,21 +2,10 @@ import React, { Component } from 'react';
 //import './Home.css';
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  handleChange(evt) {
-    // runs on every keystroke
-    this.setState({
-      fullName: evt.target.value
-    });
-  }
-
   render() {
     return (
       <div className="Home">
-        <h1>Hello Home</h1>;
+        <h1>Hello Home</h1>
       </div>
     );
   }
--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-//import './NavBar.css';
+// import './NavBar.css';
 
 class NavBar extends Component {
   render() {
@@ -11,23 +11,33 @@ class NavBar extends Component {
       };
 
       return (
-        <nav>
-          <NavLink exact to="/" activeStyle={activeStyle}>
+        <nav className="Navigation navbar sticky-top shadow-sm border-bottom navbar-expand-md">
+          <NavLink exact to="/" className="navbar-brand">
             Jobly
           </NavLink>
-          <NavLink exact to="/companies" activeStyle={activeStyle}>
-            Companies
-          </NavLink>
-          <NavLink exact to="/jobs" activeStyle={activeStyle}>
-            Jobs
-          </NavLink>
-          <NavLink exact to="/profile" activeStyle={activeStyle}>
-            Profile
-          </NavLink>
-          {/* LOGOUT does this need to be a separate component/route? */}
-          <NavLink exact to="/logout" activeStyle={activeStyle}>
-            Logout
-          </NavLink>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item mr-4">
+              <NavLink exact to="/companies" className="nav-link">
+                Companies
+              </NavLink>
+            </li>
+            <li className="nav-item mr-4">
+              <NavLink exact to="/jobs" className="nav-link">
+                Jobs
+              </NavLink>
+            </li>
+            <li className="nav-item mr-4">
+              <NavLink exact to="/profile" className="nav-link">
+                Profile
+              </NavLink>
+            </li>
+            <li className="nav-item mr-4">
+              {/* LOGOUT does this need to be a separate component/route? */}
+              <NavLink exact to="/logout" className="nav-link">
+                Logout
+              </NavLink>
+            </li>
+          </ul>
         </nav>
       );
     }
