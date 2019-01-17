da8040345a2d4ddc63f7276c12f980df83030c2d
Nick Friedland <nick.friedland@gmail.com>
2019-01-17 10:05:42 -0800

    CSS updates to jobs, nav

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -4,8 +4,10 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
-      <div className="Home">
-        <h1>Hello Home</h1>
+      <div className="Home container text-center">
+        <h1 className="mb-4 font-weight-bold">Jobly</h1>
+        <p>All the jobs in one, convenient place.</p>
+        <p>Welcome Back!</p>
       </div>
     );
   }
--- frontend/src/Jobs.js
+++ frontend/src/Jobs.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react';
-//import './Jobs.css';
+import './Jobs.css';
 import styled from 'styled-components';
 import JoblyApi from './JoblyApi.js';
+import Search from './Search.js';
+import { Link } from 'react-router-dom';
 
 class Jobs extends Component {
   constructor(props) {
     super(props);
     this.state = { jobs: [] };
+    this.searchJobs = this.searchJobs.bind(this);
   }
 
   async componentDidMount() {
-    console.log('Inside Jobs.js');
-    let jobs = JoblyApi.getJobs('/').then(res => console.log('get jobs', res));
+    console.log('Inside componentDidMount Jobs.js');
+
+    try {
+      let response = await JoblyApi.getJobs('/');
+      this.setState({ jobs: response });
+    } catch (error) {
+      throw new Error('Jobs did not mount');
+    }
+  }
+
+  async searchJobs(searchTerm) {
+    console.log('Inside searchJobs Jobs.js');
+    let response = await JoblyApi.searchJobs(searchTerm);
+    this.setState({ jobs: response });
+    try {
+    } catch (error) {
+      throw new Error('Searching for Job:  Not found');
+    }
   }
 
   render() {
     return (
-      <div className="Jobs">
-        <h1>Hello From Jobs</h1>
-        {/* <Link to={`/jobs/${id}`}>{}</Link> */}
+      <div className="pt-5">
+        <div className="Jobs col-md-8 offset-md-2">
+          <h1>Hello From Jobs</h1>
+          <Search searchFilter={this.searchJobs} />
+          {this.state.jobs.map(job => (
+            <Link to={`/jobs/${job.id}`}>
+              <div>{job.title}</div>
+            </Link>
+          ))}
+        </div>
       </div>
     );
   }
